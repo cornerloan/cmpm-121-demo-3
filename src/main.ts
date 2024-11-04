f8254f94f2de6d650b7303d5c99ffb11265e6cdb
cornerloan <connorlowe21@gmail.com>
2024-11-03 16:51:31 -0800

    L16 push

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+import "./style.css";
+
+const APP_NAME = "Temp name";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+
+const button = document.createElement("button");
+button.innerText = "temp text";
+app.append(button);
+button.addEventListener("click", function () {
+  alert("you clicked the button!");
+});
